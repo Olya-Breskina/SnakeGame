d9decf80ad71de9fffd9e50a606e768eec52e6f6
Olya-Breskina <breskina-olya@mail.ru>
2023-03-22 20:58:24 +0300

    описание окна для игры

---
+++ src/MainWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    /*
+   Окно JFrame
+   В библиотеке Swing описан класс JFrame, представляющий собой окно с рамкой и строкой заголовка
+   (с кнопками «Свернуть», «Во весь экран» и «Закрыть»).
+   Оно может изменять размеры и перемещаться по экрану.
+     */
+    public MainWindow(){
+        setTitle("Змейка");//название
+        setSize(320,345);//  размеры окна
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//действие с окном при нажатие на крестик
+        setLocation(400,400);// располодение на экране
+        setVisible(true);// видимосто окна
+    }
+    public static void main(String[] args){
+        MainWindow mw=new MainWindow();
+    }
+}
